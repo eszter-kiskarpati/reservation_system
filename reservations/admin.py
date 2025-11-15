e839eac5a2433c30c20788d084aa26ac5a173119
Keszi94 <keszi94@gmail.com>
2025-11-15 18:52:13 +0000

    Add admin interface for Reservation and Table models

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Reservation, Table
 
-# Register your models here.
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ("number", "capacity")
+    search_fields = ("number",)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "time",
+        "name",
+        "party_size",
+        "status",
+        "source"
+    )
+    list_filter = ("date", "status", "source")
+    search_fields = ("name", "email", "phone")
+    filter_horizontal = ("tables",)
